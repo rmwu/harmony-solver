56c95976e15f0f28059374eaa84beee00455247e
Rachel Wu <rmwu@mit.edu>
2016-05-21 00:34:01 -0400

    basic method specs

--- harmony.py
+++ harmony.py
@@ -28,7 +28,8 @@ class Harmony():
 
 	def __init__(self, n = 0, colors = None, swaps = None):
 		"""
-		Constructor initializes grid, which is
+		Constructor
+			initializes grid, which is
 			represented as a two-dimensional array.
 			Colors and swaps for each block are 
 			represented in a tuple of form (color, swap).
@@ -49,6 +50,10 @@ class Harmony():
 			n = 0
 
 		self.n = n
+
+		# maintain moves_left for O(1) checking ending
+		# condition; else, need O(n^2) each time
+		self.moves_left = sum(swaps)
 
 		# todo: initialize grid here
 		self.grid = []
@@ -75,7 +80,8 @@ class Harmony():
 
 	def valid_index(self, index):
 		"""
-		valid_index returns whether the given index
+		valid_index
+			returns whether the given index
 			is within the bounds of the grid
 
 		Parameters
@@ -83,15 +89,16 @@ class Harmony():
 				index of item grid[i][j]
 
 		Return
-			True	if 0 <= i, j < n
-			False	otherwise
+			True: if 0 <= i, j < n
+			False: otherwise
 		"""
 		i, j = index
 		return (0 <= i < self.n and 0 <= j < self.n)
 
 	def indices_in_line(self, index1, index2):
 		"""
-		indices_in_line returns whether the items at
+		indices_in_line
+			returns whether the items at
 			index1 and index2 can be legally swapped
 			in this grid. Legal swaps are allowed
 			between blocks directly inline with each
@@ -110,9 +117,9 @@ class Harmony():
 				the index of item grid[i2][j2]
 
 		Return
-			True	if i1 == i2 or j1 == j2 and
-					both index1 and index2 are valid
-			False	otherwise
+			True: if i1 == i2 or j1 == j2 and
+				both index1 and index2 are valid
+			False: otherwise
 		"""
 		# check if index1 and index2 are valid
 		for index in [index1, index2]:
@@ -128,13 +135,60 @@ class Harmony():
 	################################
 	# Basic logic rules for the game
 	################################
-
-	def valid_swaps(self, index):
+	def valid_swap(self, index1, index2):
 		"""
-		valid_swaps 
+		valid_swaps
+			returns whether or not a swap is valid
+			in this game, given the number of swaps
+			available
+
+		Parameters
+			index1: (i1, j1) tuple representing
+				the index of item grid[i1][j1]
+			index2: (i2, j2) tuple representing
+				the index of item grid[i2][j2]
+
+		Return
+			True: if index1, index2 are colinear
+				and the blocks at these locations
+				both have > 0 swaps available
+			False: otherwise
+		"""
+		pass
+
+	def game_solved(self):
+		"""
+		game_solved
+			returns whether or not the game of Harmony
+			has been solved. If the game is solved, there
+			are no remaining swaps left, and the colors
+			are ordered in the gradient, with color 0 at
+			the top and color n-1 at the bottom.
+
+		Return
+			True: if all swaps are 0, and the blocks are
+				in order of color by row
+			False: otherwise
+		"""
+		pass
+
+	################################
+	# Pathfinding functionality
+	################################
+	def valid_moves(self, index):
+		"""
+		valid_moves
+			returns a list of valid swapping candidates
+			from the given index. These indices must be
+			valid swaps, as defined by valid_swap.
 
 		Parameters
 			index: (i, j) tuple representing the
 				index of item grid[i][j]
+
+		Return
+			[index1, index2, ...] of valid swaps starting
+			from the given index
 		"""
+		pass
 
